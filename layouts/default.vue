aedf5a287f46acf3fe16355321be9478de7fda26
xuyan <xuyan@enmonster.com>
2019-05-16 19:59:06 +0800

    layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,117 +1,61 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title" />
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-toolbar-side-icon @click="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-content>
-      <v-container>
-        <nuxt />
-      </v-container>
-    </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>
-              compare_arrows
-            </v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
-      <span>&copy; 2019</span>
-    </v-footer>
-  </v-app>
+  <div id="app">
+    <v-app id="inspire">
+      <v-toolbar color="white" fixed app>
+        <v-toolbar-items>
+          <v-btn v-for="(nav, key) in navs" :key="key" flat>{{nav.text}}</v-btn>
+        </v-toolbar-items>
+        <v-spacer></v-spacer>
+        <v-toolbar-title right>
+          <v-img
+            :src="require('@/assets/logo.jpg')"
+            class="mr-5 logo"
+            contain
+            height="48"
+            width="48"
+            max-width="48"
+            @click="$vuetify.goTo(0)"
+          />
+        </v-toolbar-title>
+      </v-toolbar>
+      <v-content>
+        <v-container fluid fill-height>
+          <v-layout justify-center align-center>
+            <nuxt-child/>
+          </v-layout>
+        </v-container>
+      </v-content>
+      <!-- <v-footer color="shades" app inset>
+      <span class="white--text">&copy; 2017</span>
+      </v-footer>-->
+    </v-app>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
+      navs: [
         {
-          icon: 'apps',
-          title: 'Welcome',
-          to: '/'
+          text: '前端技术',
+          href: 'new'
         },
         {
-          icon: 'bubble_chart',
-          title: 'Inspire',
-          to: '/inspire'
+          text: '游戏',
+          href: 'breadcrumbs_link_1'
+        },
+        {
+          text: '电影',
+          href: 'breadcrumbs_link_2'
         }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      ]
     }
   }
 }
 </script>
+<style style="css" >
+.logo {
+  border-radius: 50%;
+}
+</style>
